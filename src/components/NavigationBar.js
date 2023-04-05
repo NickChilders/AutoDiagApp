a63b7290c50e2c7dca55e21bebb30636668fa6dd
nrchilders <74997040+nrchilders@users.noreply.github.com>
2023-04-05 12:12:07 -0400

    Added Navbar Style. Prepping new .env

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,4 @@
 import { Nav, Navbar, NavbarBrand, NavLink } from "react-bootstrap";
-import { Dash }from 'react-bootstrap-icons';
 import { BsClipboard2Check, BsWrench } from "react-icons/bs";
 import { MdManageAccounts, MdOutlineLogin, MdOutlineLogout, MdOutlineHome, MdOutlineForum } from "react-icons/md";
 import React, { useContext } from 'react';
@@ -15,13 +14,39 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls='navbarScroll' data-bs-target="#navbarScroll">
                 <div className="navbar-item">
                     {
-                        user ? (<div style={{ color: "var(--bs-nav-link-color)", fontSize: 28 }}><img src={logo} width="auto" height="40" alt="random logo" />&emsp; AutoDiag</div>) : (<div style={{ color: "var(--bs-nav-link-color)", fontSize: 28 }}><img src={logo} width="auto" height="40" alt="random logo" />&emsp;AutoDiag</div>)
+                        user ? (
+                            <div style={{ color: "var(--bs-nav-link-color)", fontSize: 28 }}>
+                                <img src={logo} width="auto" height="40" alt="random logo" />
+                                    &emsp; AutoDiag
+                            </div>
+                        ) : (
+                            <div style={{ color: "var(--bs-nav-link-color)", fontSize: 28 }}>
+                                <img src={logo} width="auto" height="40" alt="random logo" />
+                                    &emsp;AutoDiag
+                            </div>
+                        )
                     }
                 </div>
             </Navbar.Toggle>
+            <div className="ml-auto">
+                {
+                    user ? (
+                        <div className="navbar-item" style={{fontSize:20, color: "rgb(255 255 255 / 55%)"}}>
+                            {user.username}
+                            <div style={{fontSize:12}}>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="navbar-item">
+                            <NavLink eventKey="5" as={Link} style={{ fontSize:20, color: "rgb(255 255 255 / 55%)"}} to={`${process.env.PUBLIC_URL}/login`} href={`${process.env.PUBLIC_URL}/login`}>Sign in <MdOutlineLogin /></NavLink>
+                        </div>
+                    )
+                }
+            </div>
             <Navbar.Collapse id='navbarScroll'>
                 <Nav className="flex-grow-1 justify-content-evenly" style={{ width: "100%", fontSize: 24 }}>
-                    <NavbarBrand style={{ display: "flex" }}>&thinsp;<Dash /><Dash /><Dash /><Dash /><Dash /><Dash /><Dash /><Dash /><Dash /><Dash /></NavbarBrand>
+                    <NavbarBrand style={{ display: "flex" }}></NavbarBrand>
+                    <hr style={{ height: "3px", border: "none", backgroundColor: "rgb(255 255 255 / 55%)", margin: "10px 0" }} />
                     <NavLink  eventKey="0" as={Link} to={`${process.env.PUBLIC_URL}/`} href={`${process.env.PUBLIC_URL}/`} style={{ display: "flex" }}><MdOutlineHome />&emsp;Home</NavLink>
                     <NavLink eventKey="1" as={Link} to={`${process.env.PUBLIC_URL}/diagnostics`} href={`${process.env.PUBLIC_URL}/diagnostics`} style={{ display: "flex" }}><BsWrench />&emsp;Diagnostics</NavLink>
                     <NavLink eventKey="2" as={Link} to={`${process.env.PUBLIC_URL}/maintenance`} href={`${process.env.PUBLIC_URL}/maintenance`} style={{ display: "flex" }}><BsClipboard2Check />&emsp;Maintenance</NavLink>
@@ -35,7 +60,7 @@ const NavigationBar = () => {
                         ) : (
                             <div className="navbar-end">
                                 <div className="navbar-item">
-                                    <NavLink eventKey="5" as={Link} style={{ display: "flex" }} to={`${process.env.PUBLIC_URL}/login`} href={`${process.env.PUBLIC_URL}/login`}><MdOutlineLogin />&emsp;Login</NavLink>
+                                    <NavLink eventKey="5" as={Link} style={{ display: "flex" }} to={`${process.env.PUBLIC_URL}/login`} href={`${process.env.PUBLIC_URL}/login`}><MdOutlineLogin />&emsp;Sign in</NavLink>
                                 </div>
                             </div>
                         )}
